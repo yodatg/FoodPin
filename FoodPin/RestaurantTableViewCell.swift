624cf6b6ed9f3a0cc908fdd25933338380943eba
Tom Grant <bill@tommygtchbarpro.home>
2018-11-09 14:29:43 +0000

    End of Chapter 11

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Tom Grant on 23/10/2018.
+//  Copyright © 2018 Thomas Grant. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
